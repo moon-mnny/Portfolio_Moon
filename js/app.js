f1fa82b4f2278a941a38eec1cb1639f1ae2f9b5a
Moon Jo <urwisdom77@gmail.com>
2020-03-18 13:40:34 -0400

    update: Whole layout of the portfolio

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  // $("#videocontent").css({
+  //   width: $("#myVideo").width() + "px"
+  // });
+  $("#coding-project").css({
+    height: $("#myVideo").height() + "px"
+  });
+
+  // const gra = function(min, max) {
+  //   return Math.random() * (max - min) + min;
+  // };
+});
+
+// const init = function() {
+//   let items = document.querySelectorAll("section");
+//   for (let i = 0; i < items.length; i++) {
+//     items[i].style.background = randomColor({ luminosity: "light" });
+//   }
+//   cssScrollSnapPolyfill();
+// };
+// init();
